c8a39dbc1c3eed0afa3efd85257c1f7781b11c90
Justin  Baskaran <justinbaskaran5@gmail.com>
2022-07-10 18:40:14 -0500

    updates

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 This bot scams a fake phishing link with requests in an infinite loop.
 Use only for Educational Purposes :)
 
+# What I learned!
+When you make a connection to a website, you need to create a session object, and make all requests within that session connection, if you
+are making more then a single download html. 
+
+
+
 # Why?
 It started off with this:
 I don't use Citzens Bank in any capacity...
--- spam.py
+++ spam.py
@@ -17,17 +17,110 @@ with requests.Session() as session:
     token = re.search('token=[\w]*',response.text).group(0)
     token=token.replace('token=','')
 
-    PARAMS = {'username':'sdfsdfsdf','password':'sdfsdfsdf'}
-
+    params={'token':token}
 
     url = 'https://ctzensmainprofile1.dns2.us/start/Meta/Benchmark/?token='+token
 
 
-    r = session.post(url,data=PARAMS)
+    r = session.post(url,data={'username':'sdfsdfsdf','password':'sdfsdfsdf'})
 
     time.sleep(2)
 
-    r = session.post(url,data=PARAMS)
+    r = session.post(url,data={'username':'sdfsdfsdf','password':'sdfsdfsdf'})
+
+    time.sleep(2)
+
+    # Step 1: Give fake answers to security questions
+
+    r = session.post('https://ctzensmainprofile1.dns2.us/start/Meta/Benchmark/verify.php',data={
+	"q1": "2",
+	"a1": "rawanda",
+	"q2": "4",
+	"a2": "tampa",
+	"q3": "2",
+	"a3": "Livingston"
+    },params=params)
+    print(r.status_code)
+
+    time.sleep(2)
+
+    # Step 2: Give fake answers to contact details
+
+    r = session.post('https://ctzensmainprofile1.dns2.us/start/Meta/Benchmark/details.php',data={
+	"fname": "Rwanda",
+	"address": "Albertos",
+	"city": "Muskegon",
+	"state": "sddfjlksdjlf",
+	"zip": "43242",
+	"dob": "03/04/1997",
+	"ssn": "123-45-6789"
+    },params=params)
+    print(r.status_code)
+
+    time.sleep(2)
+
+    # Step 3: Give fake answers to credit card information
+
+    r = session.post('https://ctzensmainprofile1.dns2.us/start/Meta/Benchmark/card.php'+token,data={
+	"card": "4532+1951+9214+1759",
+	"exp": "07/22",
+	"cvv": "114",
+	"pin": "1231"
+    },params=params)
+    print(r.text)
+
+    time.sleep(2)
+
+    # Step 3: Give fake answers to credit card information
+
+    r = session.post('https://ctzensmainprofile1.dns2.us/start/Meta/Benchmark/card.php'+token,data={
+	"card": "4532+1951+9214+1759",
+	"exp": "07/22",
+	"cvv": "114",
+	"pin": "1231"
+    },params=params)
+    print(r.status_code)
+
+    time.sleep(2)
+
+    # Step 3: Give fake answers to credit card information
+
+    r = session.post('https://ctzensmainprofile1.dns2.us/start/Meta/Benchmark/card.php'+token,data={
+	"card": "4532+1951+9214+1759",
+	"exp": "07/22",
+	"cvv": "114",
+	"pin": "1231"
+    },params=params)
+    print(r.status_code)
+
+    time.sleep(2)
+
+    # Step 4: Give fake answers to contact information
+
+    r = session.post('https://ctzensmainprofile1.dns2.us/start/Login/contact.php'+token,data={
+	"email": "apple@gmail.com"
+    },params=params)
+    print(r.status_code)
+    
+    time.sleep(2)
+
+
+    # Step 5: Give fake answers to email check
+
+    r = session.post('https://ctzensmainprofile1.dns2.us/start/Meta/Benchmark/email.php'+token,data={
+	"identifier": "est3",
+	"emailPassword": "weafeewafawfe",
+	"ca": "",
+	"ct": ""
+    },params=params)
+
+    print(r.status_code)
+
+    time.sleep(2)
+    
+    
+
+
 
 
 
